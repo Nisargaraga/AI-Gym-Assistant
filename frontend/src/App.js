1f28e96861d06878ab719d41d92eccdd34eff835
nisargaraga <nisargarg28@gmail.com>
2026-06-19 18:26:23 +0530

    Final submission updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,16 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Auth from "./components/Auth";
 
 function App() {
-
   return (
-
-    <BrowserRouter>
-
+    <HashRouter>
       <Routes>
-
-        <Route
-          path="/"
-          element={<Auth />}
-        />
-
-        <Route
-          path="/app"
-          element={<Layout />}
-        />
-
+        <Route path="/" element={<Auth />} />
+        <Route path="/app" element={<Layout />} />
       </Routes>
-
-    </BrowserRouter>
-
+    </HashRouter>
   );
 }
 
